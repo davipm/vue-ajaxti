b2cbd62fbc2bf57ef53b3169f983a46c7b3b2516
davip <davi.p.m94@gmail.com>
2019-03-12 14:30:36 -0300

    update api

--- pages/contato/index.vue
+++ pages/contato/index.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="section page">
     <div class="container">
-      <iframe src="http://ajaxtiapi.local/contato/"
+      <iframe src="https://ajaxwebapp.azurewebsites.net/contato/"
               frameborder="0"
       ></iframe>
     </div>
